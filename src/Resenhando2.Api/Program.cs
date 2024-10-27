8038315f48a73e1f90e2d1364ac1c8dd98693736
raphaelcordon <cordon@outlook.com>
2024-10-27 14:53:44 +0100

    allowing cors to everything

--- src/Resenhando2.Api/Program.cs
+++ src/Resenhando2.Api/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         builder =>
         {
-            builder.WithOrigins("http://localhost:5173")
+            builder.WithOrigins()//"http://localhost:5173"
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
